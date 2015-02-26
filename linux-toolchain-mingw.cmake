1f7ba66a3ad96bd87e6266dd14471d0b8d04c218
Karolin Varner <karo@cupdev.net>
2015-02-26 21:15:08 +0100

    Readd mingw support; do not set libraries statically
    Before this CMakeLists.txt would statically set the library
    path and library variables for lots of libs.
    Now we should employ the auto detection based approach in
    the default CMakeFiles in the main repo.

--- linux-toolchain-mingw.cmake
+++ linux-toolchain-mingw.cmake
@@ -17,20 +17,15 @@ endif()
 
 # which compilers to use for C and C++
 find_program(CMAKE_RC_COMPILER NAMES ${COMPILER_PREFIX}-windres)
-#SET(CMAKE_RC_COMPILER ${COMPILER_PREFIX}-windres)
 find_program(CMAKE_C_COMPILER NAMES ${COMPILER_PREFIX}-gcc)
-#SET(CMAKE_C_COMPILER ${COMPILER_PREFIX}-gcc)
 find_program(CMAKE_CXX_COMPILER NAMES ${COMPILER_PREFIX}-g++)
-#SET(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-g++)
-
 
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH  /usr/${COMPILER_PREFIX})
 
 # adjust the default behaviour of the FIND_XXX() commands:
-# search headers and libraries in the target environment, search 
-# programs in the host environment
+# search headers and libraries in the target environment,
+# search programs in the host environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
