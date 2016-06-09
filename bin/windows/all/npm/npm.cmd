2139ab1ed8cb1e552492744ce0f95167d725b79e
Croydon <cr0ydon@outlook.com>
2016-06-10 01:32:55 +0200

    Fix node calling from npm

--- bin/windows/all/npm/npm.cmd
+++ bin/windows/all/npm/npm.cmd
@@ -1,12 +1,8 @@
-:: Created by npm, please don't edit manually.
-@ECHO OFF
-
-SETLOCAL
-
-SET "NODE_EXE=%~dp0\node.exe"
-IF NOT EXIST "%NODE_EXE%" (
-  SET "NODE_EXE=node"
-)
+:: modified by Inexor
+:: BE CAREFUL NOT TO BREAK THINKS HERE WHEN UPGRADING NPM
+:: NODE_EXE NEEDS TO BE DEFINED IN A WAY THAT WE ARE USING THE OUR SHIPPED VERSION OF NODE!
+:: NODE_EXE IS DEFINED IN tool/node_windows_generic_handler.bat
+:: called by tool/node_windows_generic_handler.bat
 
 SET "NPM_CLI_JS=%~dp0\node_modules\npm\bin\npm-cli.js"
 FOR /F "delims=" %%F IN ('CALL "%NODE_EXE%" "%NPM_CLI_JS%" prefix -g') DO (
