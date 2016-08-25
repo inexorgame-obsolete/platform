198a23d0d5038f82297a57552e89252397a9afb4
a_teammate <madoe3@web.de>
2016-08-25 21:38:46 +0200

    [linux/paths] rename "debug" subdirs to "debug_only"
    for clearification

--- linux_include.cmake
+++ linux_include.cmake
@@ -2,5 +2,5 @@
 message(STATUS "Using the Linux Platform file")
 
 set(target "linux")
-set(subdir_debug "debug")
-set(subdir_release "release")
+set(subdir_debug "debug_only")
+set(subdir_release "release_only")
